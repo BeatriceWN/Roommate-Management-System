5c404a87fccb3fbc5bec7e347c51680d427725bb
Beatrice Wambui <bettyneema2@gmail.com>
2025-10-16 15:57:01 +0300

    Add create_tables function to database.py

--- lib/database.py
+++ lib/database.py
@@ -4,3 +4,13 @@ CONN = sqlite3.connect("db/database.db")
 CONN.execute("PRAGMA foreign_keys = ON;")
 CURSOR = CONN.cursor()
 
+def create_tables():
+    """Create tables for roommates, chores, bills, and roommate_bills"""
+
+    CURSOR.execute("""
+        CREATE TABLE IF NOT EXISTS roommates (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            room_number TEXT
+        )
+    """)
